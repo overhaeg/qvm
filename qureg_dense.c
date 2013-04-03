6945d17aa4bfe17647097a9ba0ebb17560143b85
MacEugene <maceugene@MacEugene-Nunux.(none)>
2013-04-04 00:44:49 +0200

    modifications to libquantum

--- qureg_dense.c
+++ qureg_dense.c
@@ -1,29 +1,53 @@
+#include "qureg_dense.h"
+
+unsigned long quantum_memman(long change)
+{
+  static long mem = 0, max = 0;
+
+  mem += change;
+
+  if(mem > max)
+    max = mem;
+
+  return mem;
+}
+
 quantum_reg 
-quantum_new_qureg (MAX_UNSIGNED initval, int width)
+quantum_new_qureg (MAX_UNSIGNED initval, int size)
 {
     quantum_reg reg;
     
     /*Allocate memory*/
 
-    reg.amplitudes = calloc(width, sizeof(COMPLEX_FLOAT));
-    reg.size = width;
-    /*Mem manager?*/
+    reg.amplitudes = calloc(size, sizeof(COMPLEX_FLOAT));
+    reg.size = size;
+    quantum_memman(size * sizeof(COMPLEX_FLOAT));
 
     return reg;
 
 }
 
 
+
 /*quantum_reg
 quantum_new_qureg_size Useful?
 */        
 
-quantum_reg
+void
 quantum_delete_qureg(quantum_reg *reg)
 {
     free(reg->amplitudes);
-    /*Mem manager?*?*/
+    quantum_memman(-reg->size*sizeof(COMPLEX_FLOAT);
     reg->amplitudes = 0;
+}
+
+void
+quantum_copy_qureg(quantum_reg* src, quantum_reg* dest)
+{
+   *dst = *src;
+   dst->amplitudes =  calloc(dst->size, sizeof(COMPLEX_FLOAT));
+   quantum_memman(size*sizeof(COMPLEX_FLOAT));
+   memcpy(dst->amplitudes, src->amplitudes, src->size*sizeof(COMPLEX_FLOAT));
 }
 
 quantum_reg
@@ -41,4 +65,3 @@ quantum_kronecker (quantum_reg *reg1, quantum_reg *reg2)
    return reg;
 
 }
-
--- qureg_dense.h
+++ qureg_dense.h
@@ -1,4 +1,4 @@
-struct qu_reg
+struct qu_reg_struct
 {
   COMPLEX_FLOAT* amplitudes; 
   size_t size;      
@@ -6,7 +6,7 @@ struct qu_reg
 
 quantum_reg quantum_new_reg(MAX_UNSIGNED initval, int size);
 void quantum_delete_qureg(quantum_reg *reg);
-
+void quantum_copy_qureg(quantum_reg *src, quantum_reg dst*);
 quantum_reg quantum_kronecker(quantum_reg *reg1, quantum_reg *reg2);
 
 
