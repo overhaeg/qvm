f1556f41b01a77274237847759e44c7e8abe97fc
overhaeg <MacEugene@gmail.com>
2013-04-05 03:58:45 +0200

    test

--- qureg_dense.c
+++ qureg_dense.c
@@ -1,3 +1,5 @@
+#include "qureg_dense.h"
+    
 quantum_reg 
 quantum_new_qureg (MAX_UNSIGNED initval, int width)
 {
@@ -5,7 +7,7 @@ quantum_new_qureg (MAX_UNSIGNED initval, int width)
     
     /*Allocate memory*/
 
-    reg.amplitudes = calloc(width, sizeof(COMPLEX_FLOAT));
+    reg.amplitudes = calloc(width, sizeof(uint));
     reg.size = width;
     /*Mem manager?*/
 
@@ -18,7 +20,7 @@ quantum_new_qureg (MAX_UNSIGNED initval, int width)
 quantum_new_qureg_size Useful?
 */        
 
-quantum_reg
+void
 quantum_delete_qureg(quantum_reg *reg)
 {
     free(reg->amplitudes);
@@ -30,15 +32,15 @@ quantum_reg
 quantum_kronecker (quantum_reg *reg1, quantum_reg *reg2)
 {
    quantum_reg reg;
-
+   int i,j;
    reg.size = reg1->size + reg2->size;
-   reg.amplitudes = calloc(reg.size, sizeof(COMPLEX_FLOAT);
+   reg.amplitudes = calloc(reg.size, sizeof(uint));
    
    for(i=0; i<reg1->size;i++)
     for(j=0; j<reg2->size;j++)
-        reg.amplitudes[i*reg2->size+j]=reg1.amplitudes[i]*reg2.amplitudes[j];
+        reg.amplitudes[i*reg2->size+j]=reg1->amplitudes[i]*reg2->amplitudes[j];
    
    return reg;
 
 }
-
+
--- qureg_dense.h
+++ qureg_dense.h
@@ -1,10 +1,15 @@
-struct qu_reg
+#include <stdio.h>
+#include <stdlib.h>
+#include "config.h"
+
+
+typedef struct qu_reg
 {
-  COMPLEX_FLOAT* amplitudes; 
+  uint* amplitudes; 
   size_t size;      
 } quantum_reg;
 
-quantum_reg quantum_new_reg(MAX_UNSIGNED initval, int size);
+quantum_reg quantum_new_qureg(MAX_UNSIGNED initval, int size);
 void quantum_delete_qureg(quantum_reg *reg);
 
 quantum_reg quantum_kronecker(quantum_reg *reg1, quantum_reg *reg2);
