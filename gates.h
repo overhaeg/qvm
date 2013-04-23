45eb5fb9429a2c7dd8e3fab7581d965501179aea
overhaeg <MacEugene@gmail.com>
2013-04-23 09:33:31 +0200

    finished the conversing of the qvm

---
+++ gates.h
@@ -0,0 +1,15 @@
+//#include "qureg_dense.h"
+#include "config.h"
+
+typedef unsigned int uint;
+
+uint permute(const uint i, const uint pos, const uint size) ;
+uint alt_permute(const uint i, const uint pos, const uint size);
+uint alt2_permute(const uint i, const uint npos, const uint nsize);
+
+void base_quantum_X(const quantum_reg * const input, quantum_reg *output);
+void general_quantum_X(const quantum_reg * const input, quantum_reg *output, uint qubit_position);
+void general_quantum_Z(const quantum_reg * const input, quantum_reg *output, uint qubit_position);
+void general_quantum_CZ(int tar1, int tar2, quantum_reg * qureg);
+
+
